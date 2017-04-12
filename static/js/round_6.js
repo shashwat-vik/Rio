a1de0f5bf79c470e7d23edc6578c409dc9e75e0c
Shashwat Vikram <shashwat.vikram50@gmail.com>
2017-04-12 07:40:50 +0530

    Added Round 6, Audience and Tiebreaker 	- Verified questions for other rounds

---
+++ static/js/round_6.js
@@ -0,0 +1,70 @@
+var submit_count=0;
+var times = 1
+$(document).ready(function(e) {
+    $("#submit.click_option").click(function() {
+        submit_count += 1;
+        if (submit_count <= times) {
+            check_answer();
+            $('.tries_left h4').html('<b>'+String(times-submit_count)+'</b>');
+        }
+         else if (submit_count == (times+1)){
+             $('#submit.click_option').attr('data-target','#aModal');
+        };
+        bar.stop();
+    });
+    $('.box-icon').on('click', function () {
+        if (!timer_started) {
+            $('.box-icon .header_element').html('')
+            $('#timer.timer_container').removeAttr('hidden')
+            bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+            bar.text.style.fontSize = '2rem';
+            bar.animate(1.0, {}, function() {
+                $('#tModal').modal('show');
+            });
+            timer_started = true;
+        }
+    });
+});
+
+var check_answer = function () {
+    submission = $('.answer_box #myInput').val()
+    true_answer = $('#1.1994').attr("secanswer")
+    submission = clean_data(submission)
+    true_answer = clean_data(true_answer)
+    if (submission == true_answer) {
+        $('#submit.click_option').attr('data-target','#cModal');
+    } else {
+        $('#submit.click_option').attr('data-target','#wModal');
+    }
+};
+
+var clean_data = function(data) {
+    c_data = ""
+    data = data.toLowerCase()
+    for(var i=0; i<data.length; ++i) {
+        code = data.charCodeAt(i);
+        if((code >= 48 && code <= 57) || (code >= 97 && code <= 122))
+            c_data += data[i];
+    }
+    return c_data;
+};
+
+var timer_started = false;
+var bar = new ProgressBar.Circle(timer, {
+  color: '#fff',
+
+  strokeWidth: 4,
+  text: {
+    autoStyleContainer: false
+  },
+  duration: 10000,
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    var value = Math.round(circle.value() * 10);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+  }
+});
--- views.py
+++ views.py
@@ -2,6 +2,9 @@ from app import app
 from flask import render_template, url_for
 import os
 
+audience_q_idx = 1
+tiebreaker_q_idx = 1
+
 def generic_read_question(file_path, index):
     f = open(file_path, 'r')
     for idx, line in enumerate(f):
@@ -27,6 +30,62 @@ def all_rounds():
 @app.route("/a_e")
 def all_rounds_e():
     return render_template("all_rounds_e.html")
+
+@app.route("/audience")
+def audience():
+    global audience_q_idx
+    file_path = url_for('static', filename='questions/audience/que.txt')
+    root = os.getcwd().replace("\\","/")
+    if not len(root.split('/seed/application')) > 1:
+        root += "/seed/application"
+    file_path = root+file_path
+
+    d_next, d_prev = url_for('all_rounds')
+    if audience_q_idx > 6:
+        return render_template('first_page.html')
+    token = generic_read_question(file_path, audience_q_idx)
+    #################################################
+    data = {
+    'next': d_next,
+    'prev': d_prev,
+    'q_idx': audience_q_idx,
+    'question':token[0],
+    'option1':token[1],
+    'option2':token[2],
+    'option3':token[3],
+    'option4':token[4],
+    'answer':token[5]
+    }
+    audience_q_idx += 1
+    return render_template("round_2.html", data=data)
+
+@app.route("/tiebreaker")
+def tiebreaker():
+    global tiebreaker_q_idx
+    file_path = url_for('static', filename='questions/tiebreaker/que.txt')
+    root = os.getcwd().replace("\\","/")
+    if not len(root.split('/seed/application')) > 1:
+        root += "/seed/application"
+    file_path = root+file_path
+
+    d_next, d_prev = url_for('all_rounds')
+    if tiebreaker_q_idx > 12:
+        return render_template('first_page.html')
+    token = generic_read_question(file_path, tiebreaker_q_idx)
+    #################################################
+    data = {
+    'next': d_next,
+    'prev': d_prev,
+    'q_idx': tiebreaker_q_idx,
+    'question':token[0],
+    'option1':token[1],
+    'option2':token[2],
+    'option3':token[3],
+    'option4':token[4],
+    'answer':token[5]
+    }
+    tiebreaker_q_idx += 1
+    return render_template("round_2.html", data=data)
 
 @app.route("/1/<q_idx>")
 def round_1(q_idx):
@@ -205,3 +264,39 @@ def round_5(q_idx):
     'answer':token[3]
     }
     return render_template('round_5.html', data=data)
+
+
+@app.route("/6/<q_idx>")
+def round_6(q_idx):
+    q_idx = int(q_idx)
+    file_path = url_for('static', filename='questions/6/que.txt')
+    root = os.getcwd().replace("\\","/")
+    if not len(root.split('/seed/application')) > 1:
+        root += "/seed/application"
+    file_path = root+file_path
+
+    #################################################
+    d_next, d_prev = None, None
+    token = generic_read_question(file_path, q_idx)
+    if q_idx == 1:
+        d_prev = url_for('all_rounds')
+        d_next = url_for('round_6', q_idx=q_idx+1)
+    elif q_idx == 3:
+        d_prev = url_for('round_6', q_idx=q_idx-1)
+        d_next = url_for('all_rounds')
+    elif 1 < q_idx < 3:
+        d_prev = url_for('round_6', q_idx=q_idx-1)
+        d_next = url_for('round_6', q_idx=q_idx+1)
+    #################################################
+
+    data = {
+    'next': d_next,
+    'prev': d_prev,
+    'q_idx': q_idx,
+    'question':token[0],
+    'answer':token[1],
+    'path1': url_for('static', filename='questions/6/'+token[2]) if token[2] else None,
+    'path2': url_for('static', filename='questions/6/'+token[3]) if token[3] else None,
+    'path3': url_for('static', filename='questions/6/'+token[4]) if token[4] else None
+    }
+    return render_template("round_6.html", data=data)
